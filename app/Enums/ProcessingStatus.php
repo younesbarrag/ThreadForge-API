27d2fd523bf24f586feda83fb106b602e8e05cd3
younesbarrag <younesbarrag003@gmail.com>
2026-06-23 15:03:59 +0100

    add status enum

--- app/Enums/ProcessingStatus.php
+++ app/Enums/ProcessingStatus.php
@@ -1,10 +1,11 @@
+<?php
 
+namespace App\Enums;
 
-
-
-
-
-
-
-
-
+enum ProcessingStatus: string
+{
+    case Pending = 'pending';
+    case Processing = 'processing';
+    case Completed = 'completed';
+    case Failed = 'failed';
+}
